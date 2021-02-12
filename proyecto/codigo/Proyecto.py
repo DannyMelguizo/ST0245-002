f970b86bdc2b6cd3e6b9e519eceaed604bae6d2a
DanyMelguizo <73504424+Danymelguizo@users.noreply.github.com>
2021-02-12 17:55:51 -0500

    Add files via upload

---
+++ proyecto/codigo/Proyecto.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+
+ganado_enfermo = pd.read_csv('C:\\Users\\Usuario\\Downloads\\Universidad\\Semestre 2\\Proyecto Datos\\0.csv')
+
+
+ganado_sano = pd.read_csv('C:\\Users\\Usuario\\Downloads\\Universidad\\Semestre 2\\Proyecto Datos\\01cowburps-promo-mediumSquareAt3X-v2.csv')
+
+print(ganado_sano)
