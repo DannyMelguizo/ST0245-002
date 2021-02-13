4e1212e4e9d345af509234a68aba4ba071c1189c
DanyMelguizo <73504424+Danymelguizo@users.noreply.github.com>
2021-02-12 21:31:14 -0500

    Update Proyecto.py

--- proyecto/codigo/Proyecto.py
+++ proyecto/codigo/Proyecto.py
@@ -3,6 +3,7 @@ import pandas as pd
 
 ganado_enfermo = pd.read_csv('C:\\Users\\Usuario\\Downloads\\Universidad\\Semestre 2\\Proyecto Datos\\0.csv')
 
+print(ganado_enfermo)
 
 ganado_sano = pd.read_csv('C:\\Users\\Usuario\\Downloads\\Universidad\\Semestre 2\\Proyecto Datos\\01cowburps-promo-mediumSquareAt3X-v2.csv')
 
