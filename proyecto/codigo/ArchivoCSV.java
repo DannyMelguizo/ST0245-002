400b04d9fde3248d533b01b04b98714b063c9586
Ortegago3 <72822704+Ortegago3@users.noreply.github.com>
2021-02-12 21:06:06 -0500

    Add files via upload

---
+++ proyecto/codigo/ArchivoCSV.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.io.*;
+
+public class ArchivoCSV
+{
+    public static void main(String[] args){
+          BufferedReader br = null;
+          ArrayList<String> listaDatos = new ArrayList();
+          try {
+             Scanner myObj = new Scanner(System.in);
+             System.out.println("Ingrese el nombre de la carpeta");
+             String nombreCarpeta = myObj.nextLine();
+             System.out.println("Ingrese el nombre del archivo sin la extension");
+             String nombreArchivo = myObj.nextLine();
+             br = new BufferedReader(new FileReader("./Archivos/" + nombreCarpeta + "/" + nombreArchivo + ".csv"));
+             String line = br.readLine();
+             while (null!=line) {
+                String datos = line;
+                System.out.println(datos);
+                listaDatos.add(datos);
+                line = br.readLine();
+             }
+             
+          } catch (Exception e) {
+             System.out.println(e);
+          }
+    }
+    
+}
