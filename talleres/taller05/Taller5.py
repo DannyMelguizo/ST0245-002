e35f0a1fb64c61323ddf2910414de8d619d4fbd4
DanyMelguizo <73504424+Danymelguizo@users.noreply.github.com>
2021-02-26 16:21:44 -0500

    Update Taller5.py

--- talleres/taller05/Taller5.py
+++ talleres/taller05/Taller5.py
@@ -28,4 +28,4 @@ def insertion_sort(array):
     return array
 #print(f"La suma de los elementos en el arreglo es: {array_sum([1,2,3,4,5,6,7,8,9,])}")
 #print(f"El producto de los elementos en el arreglo es: {array_mult([1,2,3,4,5,6,7,8,9,], 5)}")
-print(f"Al hacer un insertion sort en el arreglo, queda: {insertion_sort([17,8,70,100,1000,40,5,2,1])}")
+#print(f"Al hacer un insertion sort en el arreglo, queda: {insertion_sort([17,8,70,100,1000,40,5,2,1])}")
