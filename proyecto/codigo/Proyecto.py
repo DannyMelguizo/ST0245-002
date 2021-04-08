64b6f93808d63db1ffe8d7dee7636a1313644ff6
DanyMelguizo <73504424+Danymelguizo@users.noreply.github.com>
2021-04-07 19:31:29 -0500

    Add files via upload

--- proyecto/codigo/Proyecto.py
+++ proyecto/codigo/Proyecto.py
@@ -1,10 +1,44 @@
 import numpy as np
 import pandas as pd
+from time import time
+from PIL import Image
+import PIL
+import os
+import glob
 
-ganado_enfermo = pd.read_csv('C:\\Users\\Usuario\\Downloads\\Universidad\\Semestre 2\\Proyecto Datos\\0.csv')
 
-print(ganado_enfermo)
+inicio = time()
 
-ganado_sano = pd.read_csv('C:\\Users\\Usuario\\Downloads\\Universidad\\Semestre 2\\Proyecto Datos\\01cowburps-promo-mediumSquareAt3X-v2.csv')
+def leer_ganado_enfermo(documento_csv):
+    
+    ganado_enfermo = pd.read_csv(documento_csv)
 
-print(ganado_sano)
+    print(ganado_enfermo)
+    
+    
+leer_ganado_enfermo(open('0.csv'))
+
+def leer_ganado_sano(documento_csv):
+    
+    ganado_sano = pd.read_csv(documento_csv)
+
+    print(ganado_sano)
+
+leer_ganado_sano(open('01cowburps-promo-mediumSquareAt3X-v2.csv'))
+    
+
+def compresion_imagenes_perdida(imagen_jpg):
+
+    file_name = 'compressed_image.jpg'
+    im = Image.open(imagen_jpg)
+    dim = im.size
+
+    im.save(file_name, optimize = True, quality= 10)
+
+compresion_imagenes_perdida('0.jpg')
+    
+final = time()
+
+total = final - inicio
+
+print(total)
